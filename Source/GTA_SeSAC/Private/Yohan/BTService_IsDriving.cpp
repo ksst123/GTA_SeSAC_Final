f28946ccc2509d5cbd0f51e35335b3d0699b7a04
jangdave <121008860+jangdave@users.noreply.github.com>
2023-02-11 17:52:03 +0900

    02/11

--- Source/GTA_SeSAC/Private/Yohan/BTService_IsDriving.cpp
+++ Source/GTA_SeSAC/Private/Yohan/BTService_IsDriving.cpp
@@ -5,16 +5,22 @@
 #include "Yohan/YohanCharacter.h"
 #include "AIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
 
 UBTService_IsDriving::UBTService_IsDriving()
 {
-
+	NodeName = TEXT("Is Player Driving");
 }
 
 void UBTService_IsDriving::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	AYohanCharacter* enemy = Cast<AYohanCharacter>(OwnerComp.GetAIOwner()->GetPawn());
 	AAIController* enemyAI = Cast<AAIController>(OwnerComp.GetAIOwner());
+
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	AYohanCharacter* player = Cast<AYohanCharacter>(PlayerController->GetOwner());
+
 	if (enemy == nullptr)
 	{
 		return;
@@ -25,5 +31,12 @@ void UBTService_IsDriving::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* No
 		return;
 	}
 
-	OwnerComp.GetBlackboardComponent()->SetValueAsBool(GetSelectedBlackboardKey(), enemy->bIsDriving);
+	if(player == nullptr)
+	{
+		return ;
+	}
+
+
+
+	OwnerComp.GetBlackboardComponent()->SetValueAsBool(GetSelectedBlackboardKey(), player->bIsDriving);
 }
